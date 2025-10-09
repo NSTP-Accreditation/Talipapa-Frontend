e1295097c0a86d0977dde7185ebac0eed61cb2ae
alchemist <ljcabunoc03@gmail.com>
2025-10-10 04:21:35 +0800

    add maps and fix the containers

--- src/pages/aboutus/Body.jsx
+++ src/pages/aboutus/Body.jsx
@@ -12,12 +12,12 @@ const officials = [
 
 const BarangayOfficials = () => {
   return (
-    <div className="bg-white border border-gray-200 rounded-lg p-6 md:p-[70px] max-w-6xl mx-auto mb-6 shadow-sm">
+    <div className="bg-white border border-gray-200 rounded-lg p-6 md:p-[70px] w-full shadow-sm">
       <h2 className="text-[30px] font-semibold text-green-600 mb-6 pt-[20px] pb-[50px] text-center">
         Barangay Officials
       </h2>
 
-      <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="flex flex-wrap justify-center gap-6">
         {officials.map((official, index) => (
           <OfficialCard key={index} official={official} />
         ))}
